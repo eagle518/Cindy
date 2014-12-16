a949be841c0f936a98fc02a9d7418dc99aa1dbe7
Vincent DURMONT <vdurmont@gmail.com>
2014-12-16 15:26:36 -0800

    Setting the waitDuration in the AbstractWorker when it's given as a constructor parameter

--- src/main/java/co/mindie/cindy/worker/AbstractWorker.java
+++ src/main/java/co/mindie/cindy/worker/AbstractWorker.java
@@ -11,152 +11,153 @@ import java.io.Closeable;
 
 public abstract class AbstractWorker implements Worker, Closeable, Initializable {
 
-    ///////////////////
-    // VARIABLES
-    ///////////////////
+	///////////////////
+	// VARIABLES
+	///////////////////
 
-    private static final Logger LOGGER = Logger.getLogger(AbstractWorker.class);
+	private static final Logger LOGGER = Logger.getLogger(AbstractWorker.class);
 
-    private final String workerName;
-    private final Object lock;
+	private final String workerName;
+	private final Object lock;
 
-    private boolean startOnInit;
-    private boolean stopped;
-    private boolean waitCompletionOnClose;
-    private Thread worker;
-    private TaskQueue taskQueue;
-    private TimeSpan waitDuration;
+	private boolean startOnInit;
+	private boolean stopped;
+	private boolean waitCompletionOnClose;
+	private Thread worker;
+	private TaskQueue taskQueue;
+	private TimeSpan waitDuration;
 
-    ///////////////////
-    // CONSTRUCTORS
-    ///////////////////
+	///////////////////
+	// CONSTRUCTORS
+	///////////////////
 
-    public AbstractWorker(String workerName) {
-        this(workerName, null);
-    }
+	public AbstractWorker(String workerName) {
+		this(workerName, null);
+	}
 
-    public AbstractWorker(String workerName, TimeSpan waitDuration) {
-        this.workerName = workerName;
-        this.lock = new Object();
-        this.taskQueue = new TaskQueue();
-        this.startOnInit = true;
-        this.waitCompletionOnClose = true;
-    }
+	public AbstractWorker(String workerName, TimeSpan waitDuration) {
+		this.workerName = workerName;
+		this.lock = new Object();
+		this.taskQueue = new TaskQueue();
+		this.startOnInit = true;
+		this.waitCompletionOnClose = true;
+		this.waitDuration = waitDuration;
+	}
 
-    ///////////////////
-    // WORKER LIFECYCLE
-    ///////////////////
+	///////////////////
+	// WORKER LIFECYCLE
+	///////////////////
 
-    @Override
-    public void init() {
-        if (this.startOnInit) {
-            this.start();
-        }
-    }
+	@Override
+	public void init() {
+		if (this.startOnInit) {
+			this.start();
+		}
+	}
 
-    @Override
-    public void start() {
-        this.stopped = false;
-        if (this.worker == null) {
-            Thread worker = new Thread(this::work, this.workerName + " Thread");
-            worker.setDaemon(true);
-            this.worker = worker;
-            worker.start();
-        }
-    }
+	@Override
+	public void start() {
+		this.stopped = false;
+		if (this.worker == null) {
+			Thread worker = new Thread(this::work, this.workerName + " Thread");
+			worker.setDaemon(true);
+			this.worker = worker;
+			worker.start();
+		}
+	}
 
-    private void work() {
-        while (!this.stopped) {
-            long t0 = DateTimeUtils.currentTimeMillis();
-            try {
-                LOGGER.trace(this.workerName + " running.");
-                this.run();
-                this.taskQueue.flushTasks();
-                LOGGER.trace(this.workerName + " ran in " + (DateTimeUtils.currentTimeMillis() - t0) + " ms");
-            } catch (Exception e) {
-                LOGGER.error("An error occurred while running " + this.workerName + "\n" + StackTraceUtils.stackTraceToString(e));
-            }
+	private void work() {
+		while (!this.stopped) {
+			long t0 = DateTimeUtils.currentTimeMillis();
+			try {
+				LOGGER.trace(this.workerName + " running.");
+				this.run();
+				this.taskQueue.flushTasks();
+				LOGGER.trace(this.workerName + " ran in " + (DateTimeUtils.currentTimeMillis() - t0) + " ms");
+			} catch (Exception e) {
+				LOGGER.error("An error occurred while running " + this.workerName + "\n" + StackTraceUtils.stackTraceToString(e));
+			}
 
-            try {
-                synchronized (this.lock) {
-                    if (!this.stopped) {
-                        TimeSpan waitDuration = this.getWaitDuration();
-                        if (waitDuration != null) {
-                            this.lock.wait(waitDuration.getTotalMs());
-                        }
-                    }
-                }
-            } catch (InterruptedException e) {
-                LOGGER.error("Error while waiting the worker " + this.workerName, e);
-            }
-        }
-    }
+			try {
+				synchronized (this.lock) {
+					if (!this.stopped) {
+						TimeSpan waitDuration = this.getWaitDuration();
+						if (waitDuration != null) {
+							this.lock.wait(waitDuration.getTotalMs());
+						}
+					}
+				}
+			} catch (InterruptedException e) {
+				LOGGER.error("Error while waiting the worker " + this.workerName, e);
+			}
+		}
+	}
 
-    @Override
-    public void wakeUp() {
-        synchronized (this.lock) {
-            this.lock.notifyAll();
-        }
-    }
+	@Override
+	public void wakeUp() {
+		synchronized (this.lock) {
+			this.lock.notifyAll();
+		}
+	}
 
-    @Override
-    public void stop() {
-        this.stopped = true;
-        this.wakeUp();
-        Thread thread = this.worker;
-        this.worker = null;
+	@Override
+	public void stop() {
+		this.stopped = true;
+		this.wakeUp();
+		Thread thread = this.worker;
+		this.worker = null;
 
-        if (thread != null) {
-            try {
-                thread.join();
-            } catch (InterruptedException ignored) {
-            }
-        }
-    }
+		if (thread != null) {
+			try {
+				thread.join();
+			} catch (InterruptedException ignored) {
+			}
+		}
+	}
 
-    @Override
-    public void close() {
-        LOGGER.debug(this.workerName + " closing.");
-        Thread workerThread = this.worker;
-        this.stop();
+	@Override
+	public void close() {
+		LOGGER.debug(this.workerName + " closing.");
+		Thread workerThread = this.worker;
+		this.stop();
 
-        if (this.waitCompletionOnClose && workerThread != null) {
-            try {
-                workerThread.join();
-            } catch (InterruptedException ignored) {
-            }
-        }
-    }
+		if (this.waitCompletionOnClose && workerThread != null) {
+			try {
+				workerThread.join();
+			} catch (InterruptedException ignored) {
+			}
+		}
+	}
 
-    ///////////////////
-    // GETTERS/SETTERS
-    ///////////////////
+	///////////////////
+	// GETTERS/SETTERS
+	///////////////////
 
-    public boolean isStartOnInit() {
-        return this.startOnInit;
-    }
+	public boolean isStartOnInit() {
+		return this.startOnInit;
+	}
 
-    public void setStartOnInit(boolean startOnInit) {
-        this.startOnInit = startOnInit;
-    }
+	public void setStartOnInit(boolean startOnInit) {
+		this.startOnInit = startOnInit;
+	}
 
-    public TaskQueue getTaskQueue() {
-        return this.taskQueue;
-    }
+	public TaskQueue getTaskQueue() {
+		return this.taskQueue;
+	}
 
-    public boolean isWaitCompletionOnClose() {
-        return waitCompletionOnClose;
-    }
+	public boolean isWaitCompletionOnClose() {
+		return waitCompletionOnClose;
+	}
 
-    public void setWaitCompletionOnClose(boolean waitCompletionOnClose) {
-        this.waitCompletionOnClose = waitCompletionOnClose;
-    }
+	public void setWaitCompletionOnClose(boolean waitCompletionOnClose) {
+		this.waitCompletionOnClose = waitCompletionOnClose;
+	}
 
-    public TimeSpan getWaitDuration() {
-        return waitDuration;
-    }
+	public TimeSpan getWaitDuration() {
+		return waitDuration;
+	}
 
-    public void setWaitDuration(TimeSpan waitDuration) {
-        this.waitDuration = waitDuration;
-    }
+	public void setWaitDuration(TimeSpan waitDuration) {
+		this.waitDuration = waitDuration;
+	}
 }
